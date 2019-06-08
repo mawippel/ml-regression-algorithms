6523a7ede79524d3764532b001b9ad3c7b443083
Marcelo Wippel <mawippel2@hotmail.com>
2019-06-07 22:00:53 -0300

    feat: calculating the last part

--- demo_regressaop.py
+++ demo_regressaop.py
@@ -1,6 +1,7 @@
 import scipy.io as scipy
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 data = scipy.loadmat('data_preg.mat')['data']
 
@@ -14,18 +15,85 @@ def main():
     n2 = calculateN2(x, y)
     n3 = calculateN3(x, y)
     n8 = calculateN8(x, y)
-
     plt.show()
+    plt.clf()
 
     # Calculate MSE (Mean Squared Error)
-    mse1 = np.square(np.subtract(x, n1)).mean()
-    mse2 = np.square(np.subtract(x, n2)).mean()
-    mse3 = np.square(np.subtract(x, n3)).mean()
-    mse8 = np.square(np.subtract(x, n8)).mean()
+    mse1 = calculateMSE(n1, y)
+    mse2 = calculateMSE(n2, y)
+    mse3 = calculateMSE(n3, y)
+    mse8 = calculateMSE(n8, y)
+
     print(mse1)
     print(mse2)
     print(mse3)
     print(mse8)
+
+    testData = []
+    # Get the left data as the test data
+    testData = random.choices(data, k=5)
+    # for i in range(0, 5):
+    #     testData.append(data[i])
+
+    # Get 10% of the array as the training data
+    a1_rows = set(map(tuple, data))
+    a2_rows = set(map(tuple, testData))
+    trainingData = a1_rows.difference(a2_rows)
+    trainingData = sorted(trainingData, key=takeFirst)
+
+    # Extract the trained data from the object
+    trainedX = []
+    trainedY = []
+    for tmp1 in trainingData:
+        trainedX.append(tmp1[0])
+        trainedY.append(tmp1[1])
+
+    # Extract the test data from the object
+    testX = []
+    testY = []
+    for tmp3 in range(0, len(testData)):
+        testX.append(testData[tmp3][0])
+        testY.append(testData[tmp3][1])
+
+    # Run the regression line again, but now, using the trained data
+    plt.scatter(x, y)
+    n1 = calculateN1(trainedX, trainedY)
+    betaN1 = np.polyfit(trainedX, trainedY, 1)
+    n2 = calculateN2(trainedX, trainedY)
+    betaN2 = np.polyfit(trainedX, trainedY, 2)
+    n3 = calculateN3(trainedX, trainedY)
+    betaN3 = np.polyfit(trainedX, trainedY, 3)
+    n8 = calculateN8(trainedX, trainedY)
+    betaN4 = np.polyfit(trainedX, trainedY, 8)
+    plt.show()
+    plt.clf()
+
+    aux1 = betaN1[1] + np.dot(betaN1[0], testX)
+    aux2 = np.dot(np.power(testX, 2), betaN2[0]) + np.dot(testX, betaN2[1]) + betaN2[2]
+    aux3 = np.dot(np.power(testX, 3), betaN3[0]) + np.dot(np.power(testX, 2),
+                                                    betaN3[1]) + np.dot(testX, betaN3[2]) + betaN3[3]
+    aux8 = np.dot(np.power(testX, 8), betaN4[0]) + np.dot(np.power(testX, 7), betaN4[1]) + np.dot(np.power(testX, 6), betaN4[2]) + np.dot(np.power(testX, 5), betaN4[3]) + np.dot(np.power(testX, 4), betaN4[4]) + np.dot(np.power(testX, 3), betaN4[5]) + np.dot(np.power(testX, 2),
+                                                                                                                                                                                                                                             betaN4[6]) + np.dot(testX, betaN4[7]) + betaN4[8]
+
+    mse1 = calculateMSE(aux1, testY)
+    mse2 = calculateMSE(aux2, testY)
+    mse3 = calculateMSE(aux3, testY)
+    mse8 = calculateMSE(aux8, testY)
+
+    print(mse1)
+    print(mse2)
+    print(mse3)
+    print(mse8)
+
+def calculateMSE(regression_line, y):
+    residual = 0
+    for i in range(0, len(y)):
+        residual += ((y[i] - regression_line[i])**2)
+    return residual/len(y)
+
+
+def takeFirst(elem):
+    return elem[0]
 
 
 def calculateN1(x, y):
@@ -68,6 +136,10 @@ def calculateN8(x, y):
                                                                                                                                                                                                                                              f_betas[6]) + np.dot(x, f_betas[7]) + f_betas[8]
     plt.plot(x, f, 'yellow')
     return f
+
+
+def getDiff(li1, li2):
+    return (list(set(li1) - set(li2)))
 
 
 if __name__ == "__main__":
